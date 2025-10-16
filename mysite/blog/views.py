646c0de7da32f4ed295e06e7f8cb49e825267bdf
SimoneChiodo <simochi.005@gmail.com>
2025-10-16 17:04:26 +0200

    Add templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
+# def home(request):
+#     return HttpResponse("Ciao dal mio primo progetto Django!")
 def home(request):
-    return HttpResponse("Ciao dal mio primo progetto Django!")
+    context = {"nomeUtente": "Pietro"}
+    return render(request, "home.html", context)
+
+# In python tutte le funzioni sono automaticamente esportate,
+# non c'è bisogno di un export esplicito come in JavaScript
